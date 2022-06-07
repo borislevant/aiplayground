d35b25d3912a274f1ae08fae91924de12b66c45e
Boris Levant <36374917+borislevant@users.noreply.github.com>
2022-06-08 00:05:57 +0300

    Add files via upload

---
+++ mnist_nn.py
@@ -0,0 +1,170 @@
+from sklearn.datasets import fetch_openml
+from tensorflow.keras.utils import to_categorical
+from tensorflow.keras.datasets import mnist
+from sklearn.preprocessing import OneHotEncoder, LabelEncoder
+import numpy as np
+from sklearn.model_selection import train_test_split
+import time
+
+class DeepNeuralNetwork():
+    def __init__(self, sizes, epochs=10, l_rate=0.001, filename=None):
+        self.sizes = sizes
+        self.epochs = epochs
+        self.l_rate = l_rate
+
+        # we save all parameters in the neural network in this dictionary
+        if filename is None:
+            self.params = self.initialization()
+        else:
+            self.deserialize(filename)
+
+    def serialize(self, filename):
+        np.savez(filename, **self.params)
+
+    def deserialize(self, filename):
+        self.params = dict(np.load(filename, allow_pickle=True))
+
+    def relu(self, x, derivative=False):
+        if derivative:
+            return np.where(x > 0, 1, 0)
+        return np.where(x > 0, x, 0)
+
+    def sigmoid(self, x, derivative=False):
+        if derivative:
+            return (np.exp(-x)) / ((np.exp(-x) + 1) ** 2)
+        return 1 / (1 + np.exp(-x))
+
+    def activation(self, x, derivative=False):
+        return self.relu(x, derivative)
+
+    def softmax(self, x, derivative=False):
+        # Numerically stable with large exponentials
+        exps = np.exp(x - x.max())
+        if derivative:
+            return exps / np.sum(exps, axis=0) * (1 - exps / np.sum(exps, axis=0))
+        return exps / np.sum(exps, axis=0)
+
+    def initialization(self):
+        # number of nodes in each layer
+        input_layer = self.sizes[0]
+        hidden_1 = self.sizes[1]
+        hidden_2 = self.sizes[2]
+        output_layer = self.sizes[3]
+
+        params = {
+            'W1': np.random.randn(hidden_1, input_layer) * np.sqrt(1. / hidden_1),
+            'W2': np.random.randn(hidden_2, hidden_1) * np.sqrt(1. / hidden_2),
+            'W3': np.random.randn(output_layer, hidden_2) * np.sqrt(1. / output_layer)
+        }
+
+        return params
+
+    def forward_pass(self, x_train):
+        params = self.params
+
+        # input layer activations becomes sample
+        params['A0'] = x_train
+
+        # input layer to hidden layer 1
+        params['Z1'] = np.dot(params["W1"], params['A0'])
+        params['A1'] = self.activation(params['Z1'])
+
+        # hidden layer 1 to hidden layer 2
+        params['Z2'] = np.dot(params["W2"], params['A1'])
+        params['A2'] = self.activation(params['Z2'])
+
+        # hidden layer 2 to output layer
+        params['Z3'] = np.dot(params["W3"], params['A2'])
+        params['A3'] = self.softmax(params['Z3'])
+
+        return params['A3']
+
+    def backward_pass(self, y_train, output):
+        '''
+            This is the backpropagation algorithm, for calculating the updates
+            of the neural network's parameters.
+
+            Note: There is a stability issue that causes warnings. This is
+                  caused  by the dot and multiply operations on the huge arrays.
+
+                  RuntimeWarning: invalid value encountered in true_divide
+                  RuntimeWarning: overflow encountered in exp
+                  RuntimeWarning: overflow encountered in square
+        '''
+        params = self.params
+        change_w = {}
+
+        # Calculate W3 update
+        error = 2 * (output - y_train) / output.shape[0] * self.softmax(params['Z3'], derivative=True)
+        change_w['W3'] = np.outer(error, params['A2'])
+
+        # Calculate W2 update
+        error = np.dot(params['W3'].T, error) * self.activation(params['Z2'], derivative=True)
+        change_w['W2'] = np.outer(error, params['A1'])
+
+        # Calculate W1 update
+        error = np.dot(params['W2'].T, error) * self.activation(params['Z1'], derivative=True)
+        change_w['W1'] = np.outer(error, params['A0'])
+
+        return change_w
+
+    def update_network_parameters(self, changes_to_w):
+        '''
+            Update network parameters according to update rule from
+            Stochastic Gradient Descent.
+
+            θ = θ - η * ∇J(x, y),
+                theta θ:            a network parameter (e.g. a weight w)
+                eta η:              the learning rate
+                gradient ∇J(x, y):  the gradient of the objective function,
+                                    i.e. the change for a specific theta θ
+        '''
+
+        for key, value in changes_to_w.items():
+            self.params[key] -= self.l_rate * value
+
+    def compute_accuracy(self, x_val, y_val):
+        '''
+            This function does a forward pass of x, then checks if the indices
+            of the maximum value in the output equals the indices in the label
+            y. Then it sums over each prediction and calculates the accuracy.
+        '''
+        predictions = []
+
+        for x, y in zip(x_val, y_val):
+            output = self.forward_pass(x)
+            pred = np.argmax(output)
+            predictions.append(pred == np.argmax(y))
+
+        return np.mean(predictions)
+
+    def train(self, x_train, y_train, x_val, y_val):
+        start_time = time.time()
+        for iteration in range(self.epochs):
+            for x, y in zip(x_train, y_train):
+                output = self.forward_pass(x)
+                changes_to_w = self.backward_pass(y, output)
+                self.update_network_parameters(changes_to_w)
+
+            accuracy = self.compute_accuracy(x_val, y_val)
+            print('Epoch: {0}, Time Spent: {1:.2f}s, Accuracy: {2:.2f}%'.format(
+                iteration + 1, time.time() - start_time, accuracy * 100
+            ))
+
+def preprocess_labels(y_train, y_test):
+    encoder = OneHotEncoder(sparse=False).fit(y_train.reshape((-1, 1)))
+    return encoder.transform(y_train.reshape((-1, 1))), encoder.transform(y_test.reshape((-1, 1)))
+
+if __name__ == '__main__':
+
+    (x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+    y_train, y_test = preprocess_labels(y_train, y_test)
+    x_train = (x_train.reshape((x_train.shape[0], -1)) / 255).astype('float32')
+    x_test = (x_test.reshape((x_test.shape[0], -1)) / 255).astype('float32')
+
+    dnn = DeepNeuralNetwork(sizes=[784, 128, 64, 10], epochs=5)
+    for i in range(6):
+        dnn.train(x_train, y_train, x_test, y_test)
+        dnn.serialize(f'mnist_{5*(i+1)}epochs_relu.npz')
+
